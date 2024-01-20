002e9f8c578cd48661f77c770c2ca4215e492d80
Jon Young <jey6639@gmail.com>
2024-01-20 13:53:27 -0600

    updated code for cleaner look

--- js/bookUI.js
+++ js/bookUI.js
@@ -1,22 +1,23 @@
 function renderBook(data) {
   $("#book-catelog").html("")
-}
-data = JSON.parse(data);
-console.log(data);
-
-data.items.forEach(items) => {
-  $("#book-catelog").append (
-    `<div class="book-div">
+  data = JSON.parse(data);
+  console.log(data);
+  
+  data.items.forEach((items) => {
+    $("#book-catelog").append (
+      `<div class="book-div">
       <img src="${items.imageLinks.thumbnail}"></img>
       
-    <div class="book-stuff">
+      <div class="book-stuff">
       <p class="author">${items.volumeInfo.authors}</p>
       <p class="description">${items.volumeInfo.description}</p>
-    </div>
-
-    </div>
-    `
-  )
-}
+      </div>
+      
+      </div>
+      `
+      );
+    });
+  }
+    
 
 export default renderBook;
