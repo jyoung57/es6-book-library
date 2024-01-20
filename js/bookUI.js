e4542888170dd85efea4a1da810fa7b9df3ef35a
Jon Young <jey6639@gmail.com>
2024-01-20 13:34:44 -0600

    added renderBook funtion.  Previous commit inaccurate

---
+++ js/bookUI.js
@@ -0,0 +1,22 @@
+function renderBook(data) {
+  $("#book-catelog").html("")
+}
+data = JSON.parse(data);
+console.log(data);
+
+data.items.forEach(items) => {
+  $("#book-catelog").append (
+    `<div class="book-div">
+      <img src="${items.imageLinks.thumbnail}"></img>
+      
+    <div class="book-stuff">
+      <p class="author">${items.volumeInfo.authors}</p>
+      <p class="description">${items.volumeInfo.description}</p>
+    </div>
+
+    </div>
+    `
+  )
+}
+
+export default renderBook;
